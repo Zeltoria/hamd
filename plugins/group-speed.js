321abc3321f610f8fba5db9ae147183c21e0de0a
HamBotz <hambotz71@gmail.com>
2023-02-03 14:35:26 +0700

    dikit

--- plugins/group-speed.js
+++ plugins/group-speed.js
@@ -1,5 +1,6 @@
 import { cpus as _cpus, totalmem, freemem } from 'os'
 import os from 'os'
+import fs from 'fs'
 import { performance } from 'perf_hooks'
 import { sizeFormatter } from 'human-readable'
 import { runtime } from '../lib/others.js'
@@ -18,25 +19,64 @@ let handler = async (m, { conn }) => {
 	let chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
 	let groupsIn = chats.filter(([id]) => id.endsWith('@g.us'))
 	let used = process.memoryUsage()
-	let cpu = _cpus().map(cpu => {
+	const cpus = _cpus().map(cpu => {
 		cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
 		return cpu
-	})
-	cpu = cpu[0] ? cpu[0] : ''
-	let old = performance.now()
+	  })
+	  const cpu = cpus.reduce((last, cpu, _, { length }) => {
+		last.total += cpu.total
+		last.speed += cpu.speed / length
+		last.times.user += cpu.times.user
+		last.times.nice += cpu.times.nice
+		last.times.sys += cpu.times.sys
+		last.times.idle += cpu.times.idle
+		last.times.irq += cpu.times.irq
+		return last
+	  }, {
+		speed: 0,
+		total: 0,
+		times: {
+		  user: 0,
+		  nice: 0,
+		  sys: 0,
+		  idle: 0,
+		  irq: 0
+		}
+	  })
+	  const message = m.reply('_Testing speed..._')
+	  let old = performance.now()
+	let session = fs.statSync(authFile)
 	let neww = performance.now()
-	let speed = neww - old
-	let txt = `Kecepatan Respon ${speed.toFixed(4)} detik\n\n`
-	txt += `Runtime :\n*${runtime(process.uptime())}*\n`
-	txt += `OS Uptime :\n*${runtime(os.uptime())}*\n\n`
-	txt += `💬 Status :\n- *${groupsIn.length < groups.length ? groups.length : groupsIn.length}* Group Chats\n`
-	txt += `- *${groups.length}* Groups Joined\n`
-	txt += `- *${groupsIn.length < groups.length ? 0 : groupsIn.length - groups.length}* Groups Left\n`
-	txt += `- *${chats.length - groupsIn.length}* Personal Chats\n`
-	txt += `- *${chats.length - ( groupsIn.length < groups.length ? 0 : groupsIn.length - groups.length )}* Total Chats\n\n`
-	txt += `💻 *Server Info*\n${cpu ? `_${cpu.model.trim()} (${cpu.speed} MHZ)_\n` : ''}\n`
-	txt += `RAM: ${format(totalmem() - freemem())} / ${format(totalmem())}`
-	await m.reply(txt)
+    let speed = neww - old
+    m.reply(`Kecepatan Respon ${speed.toFixed(4)} detik
+
+Runtime :\n*${runtime(process.uptime())}*
+OS Uptime :\n*${runtime(os.uptime())}*
+
+💬 Status :
+- *${groupsIn.length}* Group Chats
+- *${groupsIn.length}* Groups Joined
+- *${groupsIn.length - groupsIn.length}* Groups Left
+- *${chats.length - groupsIn.length}* Personal Chats
+- *${chats.length}* Total Chats
+
+💻 *Server Info* :
+RAM: ${format(totalmem() - freemem())} / ${format(totalmem())}
+FREE RAM: ${format(freemem())}
+SESSION SIZE: ${format(session.size)}
+PLATFORM: ${os.platform()}
+SERVER: ${os.hostname()}
+
+_NodeJS Memory Usage_
+${'```' + Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v => v.length)), ' ')}: ${format(used[key])}`).join('\n') + '```'}
+${cpus[0] ? `
+
+_Total CPU Usage_
+${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}
+
+_CPU Core(s) Usage (${cpus.length} Core CPU)_
+${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}
+`.trim())
 }
 
 handler.menugroup = ['ping']
