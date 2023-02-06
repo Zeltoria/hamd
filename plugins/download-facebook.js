8d16a1460bf755e8dfacd3f8a42f8a196e29cbd5
HamBotz <hambotz71@gmail.com>
2023-02-06 12:13:23 +0700

    hapus menu group biar ga ke ban

--- plugins/download-facebook.js
+++ plugins/download-facebook.js
@@ -1,9 +1,13 @@
-import { facebookdl, facebookdlv2 } from '@bochilteam/scraper'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://fb.watch/azFEBmFRcy/`
-    const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
-    for (const { url, isVideo } of result.reverse()) 
-    conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `🔗 *Url:* ${url}`, m)
+import { facebookDl } from './scraper.js'
+import { savefrom } from '@bochilteam/scraper'
+
+let handler = async (m, { conn, args }) => {
+	if (!args[0]) throw 'Input URL'
+	let res = await facebookDl(args[0]).catch(async _ => await savefrom(args[0])).catch(_ => null)
+	if (!res) throw 'Can\'t download the post'
+	let url = res?.url?.[0]?.url || res?.url?.[1]?.url || res?.['720p'] || res?.['360p']
+	await m.reply('_In progress, please wait..._')
+	conn.sendMessage(m.chat, { video: { url }, caption: res?.meta?.title || '' }, { quoted: m })
 }
 handler.menudownload = ['facebook <url>']
 handler.tagsdwonload = ['search']
